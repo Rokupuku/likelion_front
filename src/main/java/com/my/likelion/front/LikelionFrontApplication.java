3623bb74c94d5a4a6809e514c5440b1669fd1752
Rokupuku <rlacnd145@naver.com>
2025-04-01 20:38:38 +0900

    chapter01~02

---
+++ src/main/java/com/my/likelion/front/LikelionFrontApplication.java
@@ -0,0 +1,13 @@
+package com.my.likelion.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikelionFrontApplication.class, args);
+	}
+
+}
